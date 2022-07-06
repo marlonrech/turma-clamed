75903dc66c95cf23f7e1e8d3c53ef72ed5ec2261
raydevcp20 <rayanecristina.19@gmail.com>
2022-07-05 22:47:28 -0300

    Upload das aulas da semana 01

---
+++ Semana 02/index.js
@@ -0,0 +1,27 @@
+// var idade = window.prompt("Digite sua idade");
+// document.write(typeof idade);
+
+// var isConfirm = window.confirm();
+// document.write("<div><h2>Retorno do confirm</h2></div>");
+// document.write("<h1>" + isConfirm + "</h1>");
+// document.write(typeof isConfirm);
+
+// var isConfirm = window.confirm("Amigo realmente estou aqui?");
+// console.log("Retorno do confirm");
+// console.log("Amigo estou aqui");
+// console.log(typeof isConfirm);
+// console.log(12);
+
+// window.alert(12);
+
+
+//Exercício visto em aula - slide 17
+
+// Forma 1
+// console.log("Rayane Cristina");
+
+//Forma 2
+var nome = window.prompt("Digite o seu nome aqui:");
+var sobrenome = window.prompt("Digite seu sobrenome aqui:");
+
+console.log(nome + " " + sobrenome);
