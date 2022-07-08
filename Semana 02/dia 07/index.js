03cf62162a380a5f220eb78b8d22ed9f031fcd21
raydevcp20 <rayanecristina.19@gmail.com>
2022-07-07 23:31:43 -0300

    Acrescentando as aulas do dia 06 e 07

---
+++ Semana 02/dia 07/index.js
@@ -0,0 +1,22 @@
+// var i = 0;
+// // da linha 2 ate a linha 5 são a mesma proposta
+// i++; // Essa é a mais recomendada
+// i = i + 1;
+// i += 10;
+
+// i--;
+
+//Teste com o devTools aberto e aperte f6 para prosseguir 
+//para o próximo passo
+// for (i = 0; i < 5; i++) {
+//     debugger;
+//   console.log("contador:" + i);
+// }
+
+//Resolvendo o exercicio do slide 12
+for(contador = 1; contador <= 10 ; contador++){
+    if(contador%2 == 0){
+        document.write(contador+"<br />");
+    }
+}
+
